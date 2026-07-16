1193799e0a944c7ca8ece9de56b02f5d2e2eae55
Harsh Tadwal <harshtadwal1@gmail.com>
2026-07-16 12:31:23 +0530

    Initial Commit

---
+++ Number_guess.py
@@ -0,0 +1,37 @@
+low = 0
+high = 50
+
+random_num = int(input("Enter the random number b/w 0 and 50: "))
+
+if random_num<0 or random_num>50:
+    print("Invalid number!")
+    random_num = int(input("Enter again: "))
+
+
+print("Number Guessing Game! \nYou have five attempts!")  
+
+
+
+guess_num = int(input("Ist attempt \nGuess the number: "))
+
+if guess_num == random_num:
+    print("You won!")  
+else:
+    guess_num = int(input("2nd attempt \nGuess the number: "))
+    
+    if guess_num==random_num:
+        print("You won!")
+    else:
+        guess_num = int(input("3rt attempt \nGuess the number: "))
+        if guess_num==random_num:
+            print("You won!") 
+        else:
+            guess_num = int(input("4th attempt \nGuess the number: "))
+            if guess_num==random_num:
+                print("You won!") 
+            else:
+                guess_num = int(input("Last attempt \nGuess the number: "))
+                if guess_num==random_num:
+                    print("You won!") 
+                else:
+                    print("You lost!")
