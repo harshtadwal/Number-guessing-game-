29f413fbc075c78765ae98bb108a144816288d17
Harsh Tadwal <harshtadwal1@gmail.com>
2026-07-16 12:41:11 +0530

    Update logic

--- Number_guess.py
+++ Number_guess.py
@@ -10,28 +10,30 @@ if random_num<0 or random_num>50:
 
 print("Number Guessing Game! \nYou have five attempts!")  
 
-
-
 guess_num = int(input("Ist attempt \nGuess the number: "))
 
 if guess_num == random_num:
-    print("You won!")  
+    print("Congrats!! you guess the correct number!")  
 else:
+    print("Wrong number!")
     guess_num = int(input("2nd attempt \nGuess the number: "))
     
     if guess_num==random_num:
-        print("You won!")
+        print("Congrats!! you guess the correct number!")  
     else:
-        guess_num = int(input("3rt attempt \nGuess the number: "))
+        print("Wrong number!")
+        guess_num = int(input("3rt attempt \nGuess the number again: "))
         if guess_num==random_num:
-            print("You won!") 
+            print("Congrats!! you guess the correct number!")   
         else:
-            guess_num = int(input("4th attempt \nGuess the number: "))
+            print("Wrong number!")
+            guess_num = int(input("4th attempt \nGuess the number again: "))
             if guess_num==random_num:
-                print("You won!") 
+                print("Congrats!! you guess the correct number!")  
             else:
-                guess_num = int(input("Last attempt \nGuess the number: "))
+                print("Wrong number!")
+                guess_num = int(input("Last attempt \nGuess the number again: "))
                 if guess_num==random_num:
-                    print("You won!") 
+                    print("Congrats!! you guess the correct number!")   
                 else:
                     print("You lost!")
